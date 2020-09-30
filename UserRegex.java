c1e594c5d6d704c42816a14c53bec9c00beaf970
Suyash Jain <jain.suyash28@gmail.com>
2020-09-30 19:07:30 +0530

    Added: First Name Validator to User Registration Portal is Added

--- UserRegex.java
+++ UserRegex.java
@@ -1,6 +1,35 @@
+
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegex {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration Portal!! ");
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String name_match = "[A-Z]{1}[a-zA-Z]{2,}";			//String pattern to be detected
+
+		String name;
+		
+		//Checking for matches
+		while(true) {
+			
+			//Entering Username
+			System.out.print("Please enter your User Name: ");
+			name = sc.nextLine();
+			
+			boolean check=name.matches(name_match);							//Condition check
+			if(check) {
+				break;
+			}else{
+				System.out.println("Incorrect format. Please try again.");
+				System.out.println();
+			}
+		}
+		System.out.println("Welcome "+name);
+		System.out.println("Program End.");
 	}
 }
