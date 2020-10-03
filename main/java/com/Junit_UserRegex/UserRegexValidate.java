e895b217dcbccc2997da8e33cd1fd9aa6f730545
suyash-dce <jain.suyash28@gmail.com>
2020-10-03 12:26:12 +0530

    Modified: Exception Invalidater added to User Registration

--- main/java/com/Junit_UserRegex/UserRegexValidate.java
+++ main/java/com/Junit_UserRegex/UserRegexValidate.java
@@ -1,46 +1,73 @@
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegValidate {
-	
-	private static final String fname_match = "[A-Z]{1}[a-zA-Z]{2,}";			//String pattern to be detected
-	private static final String lname_match = fname_match;
-	private static final String email_match = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
-	private static final String phone_match = "[0-9]{1,}\\s[1-9]{1}[0-9]{9}";
-	private static final String pass_match = "((?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])).{8,}";
-	
-	public boolean validateFName(String fname) {
-		Pattern pattern=Pattern.compile(fname_match);
-		Matcher matcher=pattern.matcher(fname);
-		boolean check=pattern.matcher(fname).matches();
-		return check;
-	}
-	
-	public boolean validateLName(String lname) {
-		Pattern pattern=Pattern.compile(lname_match);
-		Matcher matcher=pattern.matcher(lname);
-		boolean check=pattern.matcher(lname).matches();
-		return check;
-	}
-	
-	public boolean validateEmail(String email) {
-		Pattern pattern=Pattern.compile(email_match);
-		Matcher matcher=pattern.matcher(email);
-		boolean check=pattern.matcher(email).matches();
-		return check;
-	}
-	
-	public boolean validatePhone(String phone) {
-		Pattern pattern=Pattern.compile(phone_match);
-		Matcher matcher=pattern.matcher(phone);
-		boolean check=pattern.matcher(phone).matches();
-		return check;
-	}
-	
-	public boolean validatePassword(String password) {
-		Pattern pattern=Pattern.compile(pass_match);
-		Matcher matcher=pattern.matcher(password);
-		boolean check=pattern.matcher(password).matches();
-		return check;
-	}
+import com.user_reg_validate.UserDetailInvalid.ExceptionType;
+
+public class UserRegexValidate {
+    
+    //Defining pattern as global variables
+    private static final String FNAME_MATCH = "[A-Z]{1}[a-zA-Z]{2,}";            //String pattern to be detected
+    private static final String LNAME_MATCH = FNAME_MATCH;
+    private static final String EMAIL_MATCH = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+    private static final String PHONE_MATCH = "[0-9]{1,}\\s[1-9]{1}[0-9]{9}";
+    private static final String PASS_MATCH = "((?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])).{8,}";
+    
+    //Function to check validity of First Name
+    public boolean validateFName(String fname) throws UserDetailInvalid {
+        Pattern pattern=Pattern.compile(FNAME_MATCH);
+        boolean check=pattern.matcher(fname).matches();
+        if(check) {
+            return check;
+        }else {
+            throw new UserDetailInvalid("First Name Invalid Exception",
+                    ExceptionType.INVALID_FIRSTNAME);
+        }
+    }
+    
+    //Function to check validity of Last Name
+        public boolean validateLName(String lname) throws UserDetailInvalid {
+            Pattern pattern=Pattern.compile(LNAME_MATCH);
+            boolean check=pattern.matcher(lname).matches();
+            if(check) {
+                return check;
+            }else {
+                throw new UserDetailInvalid("Last Name Invalid Exception",
+                        ExceptionType.INVALID_LASTNAME);
+            }
+        }
+        
+        //Function to check validity of Email
+        public boolean validateEmail(String email) throws UserDetailInvalid {
+            Pattern pattern=Pattern.compile(EMAIL_MATCH);
+            boolean check=pattern.matcher(email).matches();
+            if(check) {
+                return check;
+            }else {
+                throw new UserDetailInvalid("Email Invalid Exception",
+                        ExceptionType.INVALID_EMAIL);
+            }
+        }
+        
+        //Function to check validity of Mobile Phone number
+        public boolean validatePhone(String phone) throws UserDetailInvalid {
+            Pattern pattern=Pattern.compile(PHONE_MATCH);
+            boolean check=pattern.matcher(phone).matches();
+            if(check) {
+                return check;
+            }else {
+                throw new UserDetailInvalid("Mobile Phone Number Invalid Exception",
+                        ExceptionType.INVALID_PHONE);
+            }
+        }
+        
+        //Function to check validity of Password
+        public boolean validatePassword(String pass) throws UserDetailInvalid {
+            Pattern pattern=Pattern.compile(PASS_MATCH);
+            boolean check=pattern.matcher(pass).matches();
+            if(check) {
+                return check;
+            }else {
+                throw new UserDetailInvalid("Password Invalid Exception",
+                        ExceptionType.INVALID_PASSWORD);
+            }
+        }
 }
