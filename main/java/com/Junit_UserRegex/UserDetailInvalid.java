342c0d3ac3e9d7c5ad2f736b075a629206b057b7
suyash-dce <jain.suyash28@gmail.com>
2020-10-03 12:22:01 +0530

    ADDED: File Invalid Exceptions is Added

---
+++ main/java/com/Junit_UserRegex/UserDetailInvalid.java
@@ -0,0 +1,16 @@
+public class UserDetailInvalid extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	enum ExceptionType{								//enum code to differenciate error types
+		INVALID_FIRSTNAME,INVALID_LASTNAME,INVALID_EMAIL,
+		INVALID_PHONE,INVALID_PASSWORD;
+	}
+	ExceptionType type_of_exception;
+	public UserDetailsInvalidException(String message,ExceptionType type_of_exception) {
+		super(message);
+		this.type_of_exception=type_of_exception;
+	}
+}
